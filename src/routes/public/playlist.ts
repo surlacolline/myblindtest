d948a4876ed3d71a28cf0ecd386380bb80df87fe
Juif Elise <elise.arenillas@gmail.com>
2020-06-20 15:28:58 +0200

    Fetch Popup et ajout route

--- src/routes/public/playlist.ts
+++ src/routes/public/playlist.ts
@@ -1,26 +1,33 @@
-import { Request, Response, Router } from "express";
-import path from "path";
-import { ParamsDictionary } from "express-serve-static-core";
+import { Request, Response, Router } from 'express';
+import path from 'path';
+import { ParamsDictionary } from 'express-serve-static-core';
 
 // Init shared
 const router = Router();
 
-const viewsDir = path.join(__dirname, "../../views");
+const viewsDir = path.join(__dirname, '../../views');
 
 /******************************************************************************
  *                      Get choix playlists html - "GET /playlists"
  ******************************************************************************/
 
-router.get("", async (req: Request, res: Response) => {
-  res.sendFile("choixPlaylist.html", { root: viewsDir });
+router.get('', async (req: Request, res: Response) => {
+  res.sendFile('choixPlaylist.html', { root: viewsDir });
+});
+
+/******************************************************************************
+ *                      Get popup html - "GET /playlists"
+ ******************************************************************************/
+router.get('/popup', async (req: Request, res: Response) => {
+  res.sendFile('popup.html', { root: viewsDir });
 });
 
 /******************************************************************************
  *                      Get choix playlists html - "GET /playlists"
  ******************************************************************************/
-router.get("/:id", async (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params as ParamsDictionary;
-  res.sendFile("playlist.html", { root: viewsDir });
+  res.sendFile('playlist.html', { root: viewsDir });
 });
 
 export default router;
