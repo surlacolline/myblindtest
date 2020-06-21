1974c1bbfa56db4d7d603755476ca7878c4e78f6
Elise <elise.arenillas@gmail.com>
2020-06-21 11:30:52 +0200

    git ignore dist

--- dist/routes/public/Playlist.js
+++ dist/routes/public/Playlist.js
@@ -4,12 +4,15 @@ const tslib_1 = require("tslib");
 const express_1 = require("express");
 const path_1 = tslib_1.__importDefault(require("path"));
 const router = express_1.Router();
-const viewsDir = path_1.default.join(__dirname, "../../views");
-router.get("", (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
-    res.sendFile("choixPlaylist.html", { root: viewsDir });
+const viewsDir = path_1.default.join(__dirname, '../../views');
+router.get('', (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+    res.sendFile('choixPlaylist.html', { root: viewsDir });
 }));
-router.get("/:id", (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+router.get('/popup', (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+    res.sendFile('popup.html', { root: viewsDir });
+}));
+router.get('/:id', (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.sendFile("playlist.html", { root: viewsDir });
+    res.sendFile('playlist.html', { root: viewsDir });
 }));
 exports.default = router;
