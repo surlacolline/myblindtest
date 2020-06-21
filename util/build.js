e4a84048b40085f6e8ebdf6cd4fae61602072b83
Elise <elise.arenillas@gmail.com>
2020-06-21 15:01:24 +0200

    run tsc with package and not build

--- util/build.js
+++ util/build.js
@@ -8,13 +8,13 @@ try {
   fs.copySync('./src/public', './dist/public');
   fs.copySync('./src/views', './dist/views');
   // Transpile the typescript files
-  const proc = childProcess.exec('tsc --build tsconfig.prod.json');
-  proc.on('close', (code) => {
-    if (code !== 0) {
-      console.log(code);
-      //throw Error("Build failed");
-    }
-  });
+  // const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+  // proc.on('close', (code) => {
+  //   if (code !== 0) {
+  //     console.log(code);
+  //     //throw Error("Build failed");
+  //   }
+  // });
 } catch (err) {
   console.log(err);
 }
