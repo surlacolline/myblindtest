ad0fb96ad81865131ec242f994782299fe5e1c4f
Elise <elise.arenillas@gmail.com>
2020-06-22 08:50:12 +0200

    remettre es6-request à la dernière version

--- util/build.js
+++ util/build.js
@@ -8,7 +8,9 @@ try {
   fs.copySync('./src/public', './dist/public');
   fs.copySync('./src/views', './dist/views');
   // Transpile the typescript files
-  // const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+  // const proc = childProcess.exec('tsc --build tsconfig.prod.json', (error) => {
+  //   if (errror) throw error;
+  // });
   // proc.on('close', (code) => {
   //   if (code !== 0) {
   //     console.log(code);
