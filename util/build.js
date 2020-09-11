3bbafb6c43956ef2c7b92716ead40e59e99f99d0
Elise <elise.arenillas@gmail.com>
2020-09-11 12:27:45 -0400

    Fix: copy angular build

--- util/build.js
+++ util/build.js
@@ -6,7 +6,7 @@ try {
   fs.removeSync('./dist/');
   // Copy front-end files
   fs.copySync('./src/public', './dist/public');
-  fs.copySync('./src/views', './dist/views');
+  fs.copySync('./src/front/dist/front', './dist/front/dist/front');
   // Transpile the typescript files
   // const proc = childProcess.exec('tsc --build tsconfig.prod.json', (error) => {
   //   if (error) console.log('my log : ' + error);
